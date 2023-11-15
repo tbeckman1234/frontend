17cbfc1549baf08ae7a36c41b472a866e0a64235
Thomas <tbeckman1234@gmail.com>
2023-11-15 14:12:48 -0700

    fixed fetch URL

--- src/components/WorkoutForm.js
+++ src/components/WorkoutForm.js
@@ -22,7 +22,7 @@ export default function WorkoutForm() {
 
         const workout = {title, load, reps}
 
-        const response = await fetch('/api/workouts', {
+        const response = await fetch('https://mern-backend-tau.vercel.app/api/workouts', {
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
